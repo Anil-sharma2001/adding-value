e8ce6e17332d92d7d34202b607e1f0a87fe6aaf2
anilatwork2001 <anilatwork2001@gmail.com>
2022-02-23 14:39:55 +0530

    formated app.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import React, { Component } from "react"
-import Counters from "./component/counters"
+import React, { Component } from "react";
+import Counters from "./component/counters";
 import Nevbar from "./component/nevbar";
 
 class App extends Component {
@@ -12,12 +12,12 @@ class App extends Component {
       { id: 5, value: 0 },
     ],
   };
-  
+
   handleDelete = (count) => {
     const counters = this.state.counters.filter((c) => {
       console.log(
         c.id,
-  
+
         count
       );
       return c.id !== count;
@@ -26,9 +26,8 @@ class App extends Component {
   };
   handleReset = () => {
     const counters = this.state.counters.map((c) => {
-      
-      c.value =0;
-  
+      c.value = 0;
+
       return c;
     });
     this.setState({ counters });
@@ -36,26 +35,25 @@ class App extends Component {
   handlerEvent = (counter) => {
     const counters = [...this.state.counters];
     const index = counters.indexOf(counter);
-    counter[index]={...counter};
+    counter[index] = { ...counter };
     counters[index].value++;
     this.setState({ counters });
   };
-  render() { 
+  render() {
     return (
-    
       <div>
-       <Nevbar
-       totalCounters={this.state.counters.filter(c=>c.value>0).length}
-       />
-       <Counters 
-       onIncrement={this.handlerEvent}
-       onDelete={this.handleDelete}
-       onReset={this.handleReset}
-       counter={this.state.counters}
-       />
+        <Nevbar
+          totalCounters={this.state.counters.filter((c) => c.value > 0).length}
+        />
+        <Counters
+          onIncrement={this.handlerEvent}
+          onDelete={this.handleDelete}
+          onReset={this.handleReset}
+          counter={this.state.counters}
+        />
       </div>
     );
   }
 }
- 
+
 export default App;
